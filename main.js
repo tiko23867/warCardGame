73ececf6292bc38228be8bc1f76090d04464166e
Jaxson Reynolds <jaxson.reynolds@digintent.com>
2017-07-07 17:05:04 -0500

    Clean code and complete game untill end of deck

--- main.js
+++ main.js
@@ -1,92 +1,67 @@
 var points1 = 0;
 var points2 = 0;
-//var c1 = [];
-//var c2 = [];
-var pres1 = 0;
-var pres2 = 0;
-var i = 0;
-
-/* For making a new deck
-$.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1", deckId = function(data){
-	var deckId = JSON.stringify(data.deck_id);
-	deckId = deckId.substr(1,deckId.length - 2);
-
-	console.log(deckId);
-	*/
+var i = 1;
 
 //This expires in 2 weeks
 var deckId = '1kxx9naho1bf';
 
 $.get("https://deckofcardsapi.com/api/deck/" + deckId + "/shuffle/",function(data){
+	getCards();
+});
 
-
-	$.get("https://deckofcardsapi.com/api/deck/" + deckId + "/draw/?count=52", deckId = function(data){
-		//document.getElementById("card1").src = data.cards[0].image;
-		//document.getElementById("card2").src = data.cards[1].image;
-		var deckNum = data.remaining;
-	//console.log(deckNum);
-
-	var c1 = [];
-	var c2 = [];
-	//Makes the deck for both players
-		for (var u = 0; u < 52; u++)
-		{
-			if (u % 2 === 0 || u === 0)
-				c1.push(data.cards[u].value)
-
-			else
-				c2.push(data.cards[u].value)
-		}
-
-	var players = [c1, c2];
-
-	var faceCards = ['JACK','QUEEN','KING','ACE'];
-	var l = faceCards.length;
-
-//Converts the Face cards
-for (var x = 0; x < 26; x++)
+function getCards()
 {
-	for (var j = 0; j < 2; j++)
-	{
-		for (var h = 0; h < l; h++)
-		{
-			if (players[j][x] == faceCards[h])
-			{
-				players[j][x] = 11 + h;
-				break;
-			}
-		}
-	}
-}
+	$.get("https://deckofcardsapi.com/api/deck/" + deckId + "/draw/?count=2", function(data){
 
-//Present value
-pres1 = players[0][i];
-pres2 = players[1][i];
+		if (i === 27)
+		{
+			end();
+		}
+
+	var c1 = data.cards[0].value;
+	var c2 = data.cards[1].value;
+
+var pres1 = c1 === 'JACK' ? 11 :
+				c1 === 'QUEEN' ? 12 :
+				c1 === 'KING' ? 13 :
+				c1 === 'ACE' ? 14 :
+				c1;
+
+var pres2 = c2 === 'JACK' ? 11 :
+				c2 === 'QUEEN' ? 12 :
+				c2 === 'KING' ? 13 :
+				c2 === 'ACE' ? 14 :
+				c2;
+
 
 next(pres1, pres2);
 
-//Future value
-var f1next = players[0][i + 1];
-var f2next = players[1][i + 1];
 
-var im1 = data.cards[i].image;
-var im2 = data.cards[i +1].image;
+var im1 = data.cards[0].image;
+var im2 = data.cards[1].image;
 
 document.getElementById("card1").src = im1;
 document.getElementById("card2").src = im2;
 
-	});
-});
+console.log(data.remaining);
 
+/*
+if (data.remaining === 0)
+{
+	end();
+}*/
+
+	});
+}
 
 //Functions
-
 function next(c1, c2)
 {
 	$("#p1").html(c1);
 	$("#p2").html(c2);
 
 	$("#turn").html(i);
+	i++;
 }
 
 function compare(num1, num2)
@@ -96,7 +71,7 @@ function compare(num1, num2)
 
 	if (num1 > num2)
 	{
-		points1;
+		points1++;
 		alert("Player 1 wins!");
 	}
 
@@ -111,9 +86,15 @@ function compare(num1, num2)
 		alert("Draw!");
 	}
 
-	console.log(points1 + "vs" + points2);
-	console.log("I is " + i);
+}
 
+function end()
+{
+	var x = points1 > points2 ? "Player 1 wins with " + points1 + "s" :
+				points1 < points2 ? "Player 2 wins with " + points2 + "s" :
+				"Tie!" + points1 + "vs" + points2;
+
+	alert(x);
 }
 
 //WORD OF THE DAY!
